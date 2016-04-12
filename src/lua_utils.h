3866be1fa6228a4903d1dd865e5b13d284186416
jerluc <jeremyalucas@gmail.com>
2016-04-12 14:13:52 -0700

    [WIP] Lua utility docs

--- src/lua_utils.h
+++ src/lua_utils.h
@@ -1,7 +1,13 @@
 #ifndef LUA_UTILS_H
 #define LUA_UTILS_H
 
+// Just a few simple helpers for making it slightly
+// easier to interact with the embedded Lua bridge
+
 #ifndef LUAJIT
+// When LuaJIT is being used, we need to use
+// the lua_rawlen function instead, as lua_objlen
+// isn't defined until after Lua 5.2
 #define lua_objlen(L, I) lua_rawlen(L, I)
 #endif
 
